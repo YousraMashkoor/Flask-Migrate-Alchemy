63ca370f87f8a527310687a26f4fb1aac4821535
YousraMashkoor <yousra.mashkoor@gmail.com>
2021-03-10 17:39:52 +0500

    flask layout

---
+++ app/__init__.py
@@ -0,0 +1,18 @@
+import flask
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+DB_NAME = "test.db"
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql:////{DB_NAME}'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    db.init_app(app)
+
+    from .views import views
+    app.register_blueprint(views, url_prefix='/')
+
+
+    return app
